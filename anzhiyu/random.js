a247fa3e2b60b507ccf7d912b4ba400a9ad8aa15
lokinothing <1558500696@qq.com>
2024-08-29 23:22:27 +0800

    Site updated: 2024-08-29 23:22:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/27/C-入门详解笔记一/","2024/08/25/OpenRoadsDesigner常用键入命令/","2024/08/25/使用github和Zeabur发布网站/","2024/08/25/PicGo和Github搭建图床/","2024/08/24/使用hexo和安知鱼搭建个人博客/","2024/08/25/安知鱼主题应用注意/","2024/08/25/摄影-拍摄月亮/","2024/08/25/摄影-星河拍摄/","2024/08/25/星轨拍摄/"];function toRandomPost(){
+var posts=["2024/08/27/C-入门详解笔记一/","2024/08/25/OpenRoadsDesigner常用键入命令/","2024/08/25/PicGo和Github搭建图床/","2024/08/25/使用github和Zeabur发布网站/","2024/08/24/使用hexo和安知鱼搭建个人博客/","2024/08/25/安知鱼主题应用注意/","2024/08/25/摄影-拍摄月亮/","2024/08/25/摄影-星河拍摄/","2024/08/25/星轨拍摄/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
